f3ff0cf68caec1a93c6ea90764bfe48b795c7d26
vithushan19 <vithushan19@gmail.com>
2021-03-11 12:23:26 -0500

    Update data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ontario Vaccine Tracker`,
+    description: `Find locations with vaccines that are closest to you`,
+    author: `@vithushan19`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,25 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <SEO title="Ontario Vaccines" />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      This a list of select pharmacies that are offering the
+      @AstraZeneca/COVISHIELD vaccine in Ontario. Most of these pharmacies are
+      only accepting appointments only. If there is any issues with the data
+      displayed below please{" "}
+      <a href="mailto:vithushan19@gmail.com">contact me</a> and let me know.
     </p>
+    <iframe
+      width="100%"
+      height="520"
+      frameborder="0"
+      src="https://vithushan19.carto.com/builder/c32f064f-6d14-4852-8131-e3e210f55298/embed"
+      allowfullscreen
+      webkitallowfullscreen
+      mozallowfullscreen
+      oallowfullscreen
+      msallowfullscreen
+    ></iframe>
   </Layout>
 )
 
